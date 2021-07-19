3d1db49302b5b9f3f9eb9d14037c84cfe726f9dc
AnnaE <annaefitzgerald@gmail.com>
2021-07-18 20:03:11 -0600

    added new readone.html, edit_customer.html, show, edit, and delete buttons

--- customers.py
+++ customers.py
@@ -1,5 +1,7 @@
+# import the function that will return an instance of a connection
 from mysqlconnection import connectToMySQL
 
+# model the class after the table from our database
 class Customers():
     
     def __init__(self, data):
@@ -9,25 +11,51 @@ class Customers():
         self.email_address = data['email_address']
         self.created_at = data['created_at']
         self.updated_at = data['updated_at']
-        
+
+    # Use class methods to query the database   
     @classmethod
     def new_customer(cls, data):
         
         query = "INSERT INTO customers (first_name, last_name, email_address) VALUES (%(first_name)s, %(last_name)s, %(email_address)s);"
-        
+        # Call the connectToMySQL function with the targeted schema db
         return connectToMySQL('users').query_db(query, data)
-        
+        # returns the int of id number 
 
     @classmethod 
-    def display_list(cls):
+    def get_all_customers(cls):
         
         query = "SELECT * FROM customers;"
         
         results = connectToMySQL('users').query_db(query)
-        
+        # Create an empty list to append our instances of customers
         customers = []
-        
+        # Iterate over the db results and create instances of customers with cls
         for customer in results:
             customers.append(cls(customer))
 
         return customers
+    
+    @classmethod
+    def get_customer_by_id(cls,data):
+        
+        query = "SELECT * FROM customers WHERE id = %(id)s;"
+        
+        result = connectToMySQL('users').query_db(query,data)
+        # customer is equal to the dictionary row pulled from database
+        customer = Customers(result[0])
+
+        return customer
+
+    @classmethod
+    def delete_one(cls,data):
+        
+        query = "DELETE FROM customers WHERE id = %(id)s;"
+        
+        connectToMySQL('users').query_db(query, data)
+    
+    @classmethod
+    def update_one(cls, data):
+        
+        query = "UPDATE customers SET first_name = %(first_name)s, last_name = %(last_name)s, email_address = %(email_address)s WHERE id = %(id)s;"
+        
+        connectToMySQL('users').query_db(query, data)
--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, redirect, session, request
+# import the class from customers.py
 from customers import Customers
 
 app = Flask(__name__)
@@ -7,34 +8,69 @@ app.secret_key = "new store"
 
 @app.route('/')
 def index():
-    #landpage click to add customer or view account database
     return render_template('index.html')
 
 
 @app.route('/form')
 def form():
-    #form to enter new customer
     return render_template('create.html')
 
 
-@app.route('/customers/create', methods = ['POST'] )
+@app.route('/create',methods= ['POST'])
 def create_customers():
-    #data posted to database and new customer account created
     data = {
         'first_name': request.form['first_name'],
         'last_name': request.form['last_name'],
         'email_address': request.form['email_address']   
     }
-    Customers.new_customer(data)
-    #redirect display of customer database
-    return redirect('/display')
+    Customers.new_customer(request.form)
+    return redirect('/list_customers')
 
 
-@app.route('/display')
-def display_customers():
-    customers = Customers.display_list()
+@app.route('/list_customers')
+def display_all_customers():
+    customers = Customers.get_all_customers()
     return render_template('readall.html', customers = customers)
-    
+
+
+@app.route('/select/<int:customer_id>/show')
+def display_one_customer(customer_id):
+    data = {
+        'id' : customer_id
+    }
+    customer = Customers.get_customer_by_id(data)
+    return render_template('readone.html', customer = customer)
+
+
+@app.route('/select/<int:customer_id>/edit')
+def edit_customer(customer_id):
+    data = {
+        'id': customer_id
+    }
+    customer = Customers.get_customer_by_id(data)
+    return render_template('edit_customer.html', customer = customer)
+
+
+@app.route('/select/<int:customer_id>/update',methods= ['POST']) 
+def update_customer(customer_id):
+    data = {
+        'id': customer_id,
+        'first_name': request.form['first_name'],
+        'last_name': request.form['last_name'],
+        'email_address': request.form['email_address']   
+    }
+    Customers.update_one(data)
+    return redirect('/select/<int:customer_id>/show')
+
+
+@app.route('/select/<int:customer_id>/delete')
+def delete_customer(customer_id):
+    data = {
+        'id': customer_id
+    }
+    Customers.delete_one(data)
+    return redirect('/')
+
     
 if __name__=="__main__":
     app.run(debug = True)
